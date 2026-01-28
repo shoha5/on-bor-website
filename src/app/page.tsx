b029f52cbc88bb6ee11bfda18b4c840be712a84c
shoha5 <105639717+shoha5@users.noreply.github.com>
2026-01-28 14:44:41 +0300

    Create main page component that imports all sections.

---
+++ src/app/page.tsx
@@ -0,0 +1,19 @@
+import HeroSection from './sections/HeroSection';
+import ProblemSection from './sections/ProblemSection';
+import TwoProductsSection from './sections/TwoProductsSection';
+import MoatSection from './sections/MoatSection';
+import RoadmapSection from './sections/RoadmapSection';
+
+const Page = () => {
+    return (
+        <div>
+            <HeroSection />
+            <ProblemSection />
+            <TwoProductsSection />
+            <MoatSection />
+            <RoadmapSection />
+        </div>
+    );
+};
+
+export default Page;
